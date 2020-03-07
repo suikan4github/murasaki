0ac156021870571830d0ae2f5fe05954bec87e17
Suikan <26223147+suikan4github@users.noreply.github.com>
2020-03-07 17:20:04 +0900

    Issue #105 : Build fail if EXTI_LINE_## is equal to the ##th power of 2
    https://github.com/suikan4github/murasaki/issues/105
    
    In case of the EXTI_LINE_N is the Nth power of 2, ommit one case, to
    avoid the compile error.

--- Src/exti.cpp
+++ Src/exti.cpp
@@ -31,85 +31,117 @@ Exti::Exti(unsigned int line)
     HAL_StatusTypeDef stat;
 
     switch (line) {
+#if (EXTI_LINE_0 != 1 )
         case 1:  // fall through to next line
+#endif
         case EXTI_LINE_0:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_0);
             line_ = 1;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_0);
             break;
+#if (EXTI_LINE_1 != 2 )
         case 2:  // fall through to next line
+#endif
         case EXTI_LINE_1:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_1);
             line_ = 2;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_1);
             break;
+#if (EXTI_LINE_2 != 4 )
         case 4:  // fall through to next line
+#endif
         case EXTI_LINE_2:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_2);
             line_ = 4;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_2);
             break;
+#if (EXTI_LINE_3 != 8 )
         case 8:  // fall through to next line
+#endif
         case EXTI_LINE_3:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_3);
             line_ = 8;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_3);
             break;
+#if (EXTI_LINE_4 != 16 )
         case 16:  // fall through to next line
+#endif
         case EXTI_LINE_4:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_4);
             line_ = 16;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_4);
             break;
+#if (EXTI_LINE_5 != 32 )
         case 32:  // fall through to next line
+#endif
         case EXTI_LINE_5:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_5);
             line_ = 32;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_5);
             break;
+#if (EXTI_LINE_6 != 64 )
         case 64:  // fall through to next line
+#endif
         case EXTI_LINE_6:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_6);
             line_ = 64;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_6);
             break;
+#if (EXTI_LINE_7 != 128 )
         case 128:  // fall through to next line
+#endif
         case EXTI_LINE_7:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_7);
             line_ = 128;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_7);
             break;
+#if (EXTI_LINE_8 != 256 )
         case 256:  // fall through to next line
+#endif
         case EXTI_LINE_8:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_8);
             line_ = 256;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_8);
             break;
+#if (EXTI_LINE_9 != 512 )
         case 512:  // fall through to next line
+#endif
         case EXTI_LINE_9:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_9);
             line_ = 512;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_9);
             break;
+#if (EXTI_LINE_10 != 1024 )
         case 1024:  // fall through to next line
+#endif
         case EXTI_LINE_10:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_10);
             line_ = 1024;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_10);
             break;
+#if (EXTI_LINE_11 != 2048 )
         case 2048:  // fall through to next line
+#endif
         case EXTI_LINE_11:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_11);
             line_ = 2048;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_11);
             break;
+#if (EXTI_LINE_12 != 4096 )
         case 4096:  // fall through to next line
+#endif
         case EXTI_LINE_12:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_12);
             line_ = 4096;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_12);
             break;
+#if (EXTI_LINE_13 != 8192 )
         case 8192:  // fall through to next line
+#endif
         case EXTI_LINE_13:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_13);
             line_ = 8192;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_13);
             break;
+#if (EXTI_LINE_14 != 16384 )
         case 16384:  // fall through to next line
+#endif
         case EXTI_LINE_14:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_14);
             line_ = 16384;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_14);
             break;
+#if (EXTI_LINE_15 != 32768 )
         case 32768:  // fall through to next line
+#endif
         case EXTI_LINE_15:
-            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_15);
             line_ = 32768;
+            stat = HAL_EXTI_GetHandle(&hexti_, EXTI_LINE_15);
             break;
         default:
             MURASAKI_SYSLOG(this, kfaExti, kseError, "Unexpected line. Only 0 to 15 are supported")
