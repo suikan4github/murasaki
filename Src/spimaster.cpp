20582f623b8318946c5d1bd51957423af84e40fb
Suikan <26223147+suikan4github@users.noreply.github.com>
2020-03-13 18:14:13 +0900

    Issue 112 : Add more parameter check to the SPI master and slave
    https://github.com/suikan4github/murasaki/issues/112

--- Src/spimaster.cpp
+++ Src/spimaster.cpp
@@ -29,6 +29,23 @@ SpiMaster::SpiMaster(SPI_HandleTypeDef * spi_handle):
     MURASAKI_ASSERT(nullptr != sync_)
     MURASAKI_ASSERT(nullptr != critical_section_)
 
+    // We need DMA
+    MURASAKI_ASSERT(peripheral_->hdmatx != nullptr)
+    MURASAKI_ASSERT(peripheral_->hdmarx != nullptr)
+
+
+    // For all I2S transfer, the data size have to be byte
+    MURASAKI_ASSERT(peripheral_->hdmatx->Init.PeriphDataAlignment == DMA_PDATAALIGN_BYTE)
+    MURASAKI_ASSERT(peripheral_->hdmarx->Init.PeriphDataAlignment == DMA_PDATAALIGN_BYTE)
+    MURASAKI_ASSERT(peripheral_->hdmatx->Init.MemDataAlignment == DMA_MDATAALIGN_BYTE)
+    MURASAKI_ASSERT(peripheral_->hdmarx->Init.MemDataAlignment == DMA_MDATAALIGN_BYTE)
+
+    // The DMA mode have to be circular.
+    MURASAKI_ASSERT(peripheral_->hdmatx->Init.Mode == DMA_NORMAL)
+    MURASAKI_ASSERT(peripheral_->hdmarx->Init.Mode == DMA_NORMAL)
+
+
+
 }
 
 SpiMaster::~SpiMaster()
