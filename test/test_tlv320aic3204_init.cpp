2bbb54a7a42f0476c9cb010d7c06804eef4c3f43
Suikan <26223147+suikan4github@users.noreply.github.com>
2023-08-26 17:08:24 +0900

    Studying how to test the array parameter.

--- test/test_tlv320aic3204_init.cpp
+++ test/test_tlv320aic3204_init.cpp
@@ -2,30 +2,37 @@
 
 #include "tlv320aic3204_mock.hpp"
 
-using ::testing::AtLeast;
-using ::testing::_;
+using namespace ::testing;
 
+TEST(Tlv320aic3204, init) {
+  MockI2c i2c;
+  const uint8_t table[] = {1, 2, 3, 4};
 
-TEST(Tlv320aic3204, init)
-{
-    MockI2c i2c;
-    const uint8_t table[] = {1,2,3,4};
+  murasaki::Tlv320aic3204 codec(48000,     // 48kHz.
+                                12000000,  // 12MHz
+                                &i2c,      // Through I2C master controller
+                                0x12       // At I2C address.
+  );
 
-
-    murasaki::Tlv320aic3204 codec(
-        48000,      // 48kHz.
-        12000000,   // 12MHz
-        &i2c,       // Through I2C master controller
-        0x12        // At I2C address. 
-    );
-
+#if 0
     EXPECT_CALL(i2c, Transmit(0x12,
                                 table, 
                                 sizeof(table),
                                 NULL,
                                 murasaki::kwmsIndefinitely)).Times(1);
+#else
+  EXPECT_CALL(
+      i2c,            // Mock
+      Transmit(0x12,  // I2C Address
+               _,     // Pointer to the data to send.
+               _,     // Lenght of data in bytes.
+               NULL,  // no variable to receive the length of transmission
+               murasaki::kwmsIndefinitely  // Wait forever
+               ))
+      .With(Args<1, 2>(ElementsAreArray({1, 2, 3, 4})))
+      .Times(1);
 
-    codec.SendCommand(table, sizeof(table));
+#endif
 
+  codec.SendCommand(table, sizeof(table));
 }
-
