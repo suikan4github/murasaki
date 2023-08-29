ccc3fbd7da3ae550011b7cd518c3f199e8c437b8
Suikan <26223147+suikan4github@users.noreply.github.com>
2023-08-29 09:12:35 +0900

    Add reset function test.

--- test/test_tlv320aic3204_init.cpp
+++ test/test_tlv320aic3204_init.cpp
@@ -40,3 +40,42 @@ TEST(Tlv320aic3204, SetPage) {
 
   codec.SetPage(255);
 }
+
+// Testing Reset command .
+TEST(Tlv320aic3204, Reset) {
+  MockI2cMaster i2c;
+
+  murasaki::Tlv320aic3204 codec(48000,     // 48kHz.
+                                12000000,  // 12MHz
+                                &i2c,      // Through I2C master controller
+                                0x18       // At I2C address.
+  );
+
+  // Must set page 0
+  Expectation page_set =
+      EXPECT_CALL(
+          i2c,            // Mock
+          Transmit(0x18,  // I2C Address
+                   _,     // Args<1> : Pointer to the data to send.
+                   2,     // Args<2> : Lenght of data in bytes.
+                   NULL,  // no variable to receive the length of transmission
+                   murasaki::kwmsIndefinitely  // Wait forever
+                   ))
+          .With(Args<1, 2>(ElementsAreArray({0, 0})))
+          .Times(1);
+
+  // Then write 1 to register 1.
+  EXPECT_CALL(
+      i2c,            // Mock
+      Transmit(0x18,  // I2C Address
+               _,     // Args<1> : Pointer to the data to send.
+               2,     // Args<2> : Lenght of data in bytes.
+               NULL,  // no variable to receive the length of transmission
+               murasaki::kwmsIndefinitely  // Wait forever
+               ))
+      .With(Args<1, 2>(ElementsAreArray({1, 1})))
+      .Times(1)
+      .After(page_set);
+
+  codec.Reset();
+}
