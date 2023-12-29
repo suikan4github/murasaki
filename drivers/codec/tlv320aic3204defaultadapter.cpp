b8ceb48130cfc4800e20cf89f5a1f663af91789f
Suikan <26223147+suikan4github@users.noreply.github.com>
2023-12-29 17:34:07 +0900

    Programming Tlv320aic3204DefaultAdapter::ConfigureAnalog

--- drivers/codec/tlv320aic3204defaultadapter.cpp
+++ drivers/codec/tlv320aic3204defaultadapter.cpp
@@ -28,6 +28,16 @@ Tlv320aic3204DefaultAdapter::Tlv320aic3204DefaultAdapter(
 
 void Tlv320aic3204DefaultAdapter::ConfigureAnalog(void) {
   CODEC_SYSLOG("Enter. %s", "")
+  /*
+   * HP output startup sequence ( SLAA577 section 2.2.3.1)
+   * 1. Setup Rpop and N and softstepping.
+   * 2. Configure the output driver ( common-mode, power routing)
+   * 3. Choose signal to route to HP.
+   * 4. Start up the driver block. Keep muting these blocks.
+   * 5. Unmute HPL and HPR
+   * 6. TUrn the HPL HPR driver power on.
+   * 7. After the completion of the soft-stepping, Unmute the signal source.
+   */
 
   CODEC_SYSLOG("Leave.%s", "")
 }
--- drivers/codec/tlv320aic3204defaultadapter.hpp
+++ drivers/codec/tlv320aic3204defaultadapter.hpp
@@ -14,6 +14,23 @@
 
 namespace murasaki {
 
+/**
+ * Configure the analog input and output circuit as default.
+ *
+ * Input configuration is as followings.
+ * @li IN1_L : Line Input 1 Left.
+ * @li IN1_R : Line Input 1 Right.
+ * @li IN2_L : Line Input 2 Left Positive.
+ * @li IN2_R : Line Input 2 Left Negative.
+ * @li IN3_L : Line Input 2 Right Positive
+ * @li IN3_R : Line Input 2 Right Negative.
+ *
+ * Output configuration is as followings.
+ * @li HPL : Headphone Output Left.
+ * @li HPR : Headphone Output Right.
+ * @li LOL : Line Output L
+ * @li LOR : Line Output R
+ */
 class Tlv320aic3204DefaultAdapter : public Tlv320aic3204AdapterStrategy {
  public:
   // Disabling default constructor.
@@ -29,6 +46,17 @@ class Tlv320aic3204DefaultAdapter : public Tlv320aic3204AdapterStrategy {
       u_int8_t device_address                   // 7bit I2C address.
   );
 
+  /**
+   * @brief Configure the analog input and output.
+   * @details
+   * HP Out is configured as stereo single ended.
+   *
+   * Set HP Out ( SLAA577 section 2.2.3.1 ):
+   * @li Rpop to 25kOhm ( Assume Rload is 32ohm ).
+   * @li Slow charging time N = 5.0 (Assume CC is 47uF ).
+   * @li Soft stepping time is 200mS.
+   *
+   */
   virtual void ConfigureAnalog(void);
 };  // class
 
