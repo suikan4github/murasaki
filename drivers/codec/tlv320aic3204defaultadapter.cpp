189de3c459415c358429f6ed857527b14545bc52
Suikan <26223147+suikan4github@users.noreply.github.com>
2024-01-07 09:59:15 +0900

    Programming ConfigureAnalog()

--- drivers/codec/tlv320aic3204defaultadapter.cpp
+++ drivers/codec/tlv320aic3204defaultadapter.cpp
@@ -28,6 +28,16 @@ Tlv320aic3204DefaultAdapter::Tlv320aic3204DefaultAdapter(
 
 void Tlv320aic3204DefaultAdapter::ConfigureAnalog(void) {
   CODEC_SYSLOG("Enter. %s", "")
+
+  /*
+   * Leave Page 1 / Register 10 common mode Output setting as default.
+   * Full chip common mode : 0.9v
+   * Both HPL and HPR is same as full chip common mode.
+   * LOL and LOR is same as full chip common mode.
+   * Output HPL and HPR is powered with AVDD.
+   *
+   */
+
   /*
    * HP output startup sequence ( SLAA577 section 2.2.3.1)
    * 1. Setup Rpop and N and soft stepping.
@@ -38,6 +48,57 @@ void Tlv320aic3204DefaultAdapter::ConfigureAnalog(void) {
    * 6. TUrn the HPL HPR driver power on.
    * 7. After the completion of the soft-stepping, Unmute the signal source.
    */
+
+  // Analog circuit is page 1.
+  SetPage(1);
+
+  {
+    // HPL routing selection register
+    uint8_t reg12 = static_cast<uint8_t>(
+        1 << 3 |  // Left channel DAC positive terminal is routed.
+        0);
+
+    uint8_t command[] = {
+        12,  // Register Address => 12: HPL Routing Selection register
+        reg12};
+    SendCommand(command, sizeof(command));  // Write to set gain.
+  }
+
+  {
+    // HPR routing selection register
+    uint8_t reg13 = static_cast<uint8_t>(
+        1 << 3 |  // Right channel DAC positive terminal is routed.
+        0);
+
+    uint8_t command[] = {
+        13,  // Register Address => 13: HPR Routing Selection register
+        reg13};
+    SendCommand(command, sizeof(command));  // Write to set gain.
+  }
+
+  {
+    // LOL routing selection register
+    uint8_t reg14 =
+        static_cast<uint8_t>(1 << 3 |  // Left channel DAC terminal is routed.
+                             0);
+
+    uint8_t command[] = {
+        14,  // Register Address => 14: LOL Routing Selection register
+        reg14};
+    SendCommand(command, sizeof(command));  // Write to set gain.
+  }
+
+  {
+    // LO4 routing selection register
+    uint8_t reg14 =
+        static_cast<uint8_t>(1 << 3 |  // Right channel DAC terminal is routed.
+                             0);
+
+    uint8_t command[] = {
+        14,  // Register Address => 14: LOL Routing Selection register
+        reg14};
+    SendCommand(command, sizeof(command));  // Write to set gain.
+  }
 
   CODEC_SYSLOG("Leave.%s", "")
 }
